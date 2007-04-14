e6660464016d6270ee45dff68601c5cba237597c
stephen.duncan <stephen.duncan@f191c422-131a-0410-8d44-f1ddf0d51770>
2007-04-14 22:27:29 +0000

    Made formula and size optional

--- lib/truthtable.rb
+++ lib/truthtable.rb
@@ -10,13 +10,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+require 'set'
+
 class TruthTable
   VERSION = "1.0.0"
 
-  def initialize(formula, size, &expression)
+  def initialize(formula, size = nil, &expression)
     @formula = formula
     @size = size
     @expression = expression
+    
+    @size ||= Set.new(@formula.gsub(/[^[:alpha:]]/, "").split(//)).size
+    @expression ||= eval("Proc.new {|a,b| #{formula} }")
     
     @string = ""
     
